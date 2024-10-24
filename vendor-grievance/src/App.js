509875db07805d2628b8a6df934c981b90669d19
manishamitta <mittamanisha1@gmail.com>
2024-10-24 15:29:37 +0530

    changes4

---
+++ vendor-grievance/src/App.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import FirstPage from './components/FirstPage';
+import ComplaintDetails from './components/ComplaintDetails';
+import './App.css';
+import SelectedPoState from './context/Selectedpo/SelectedPoState';
+import ComplaintState from './context/Complaint/ComplaintState';
+
+function App() {
+
+  return (
+    <SelectedPoState>
+      <ComplaintState>
+        <FirstPage />
+      </ComplaintState >
+    </SelectedPoState>
+  );
+}
+
+export default App;
